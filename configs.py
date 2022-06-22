e906274bf6cd8837189af3c2e330bf0691e07de3
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2022-06-22 08:03:50 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -32,9 +32,11 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 🧑🏻‍💻 **Developer:** @AbirHasan2005
 
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
+🧑🏻‍ **Bot Owner:** @JAsuran123
 
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+👥 **Support Group:** [Linux Repositories](https://t.me/JAsuranBotsgroup)
+
+📢 **Updates Channel:** [Discovery Projects](https://t.me/JAsuranBots)
 """
 	ABOUT_DEV_TEXT = f"""
 🧑🏻‍💻 **Developer:** @AbirHasan2005
@@ -43,7 +45,7 @@ Developer is Super Noob. Just Learning from Official Docs. Please Donate the dev
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+[Donate Now](https://www.paypal.me) (PayPal)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
