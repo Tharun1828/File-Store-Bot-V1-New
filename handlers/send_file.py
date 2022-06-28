8479b44e773bca4e95a399bc630f42439a3a5a49
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2022-06-29 04:38:54 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -6,6 +6,7 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
+import time
 
 
 async def reply_forward(message: Message, file_id: int):
@@ -37,3 +38,4 @@ async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
    # await reply_forward(message=sent_message, file_id=file_id)
     await asyncio.sleep(2)
+    await sent_message.delete()
