e6d7e97a76d103f77262fa1d9593c24c558ab8ee
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2022-07-21 12:15:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,33 +19,33 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
-Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
+**This is Permanent Files Store Bot V4!
+Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.**
 
-🤖 **My Name:** [HMTD Files Store Bot V4](https://t.me/{BOT_USERNAME})
+🤖 **My Name :** **[HMTD Files Store Bot V4](https://t.me/{BOT_USERNAME})**
 
-📝 **Language:** [Python3](https://www.python.org)
+📝 **Language :** **[Python3](https://www.python.org)**
 
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
+📚 **Library :** **[Pyrogram](https://docs.pyrogram.org)**
 
-📡 **Hosted on:** [Heroku](https://heroku.com)
+📡 **Hosted on :** **[Heroku](https://heroku.com)**
 
-🧑🏻‍💻 **Bot Owner:** @HMTD_Channels_Owner
+🧑🏻‍💻 **Bot Owner :** **@HMTD_Channels_Owner**
 
-🧑🏻‍ **Feedback:** @HMTD_Feedback_Bot
+🧑🏻‍ **Feedback :** **@HMTD_Feedback_Bot**
 
-👥 **Discussion Group:** [HMTD Discussion Group](https://t.me/HMTD_Discussion_Group)
+👥 **Discussion Group :** **[HMTD Discussion Group](https://t.me/HMTD_Discussion_Group)**
 
-📢 **Updates Channel:** [HMTD Links](https://t.me/HMTD_Links)
+📢 **Updates Channel :** **[HMTD Links](https://t.me/HMTD_Links)**
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @HMTD_Channels_Owner
+🧑🏻‍💻 **Developer :** **@HMTD_Channels_Owner**
 
-Developer is Best Movie Uploader. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
+**Developer is Best Movie Uploader. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/KarthiK717) (PayPal)
+[Donate Now](https://www.paypal.me/KarthiK717) (PayPal)**
 """
 	HOME_TEXT = """
 **Hi👋, [{}](tg://user?id={})\n\nThis is Permanent HMTD Files Store Bot V4.
