b26e33c9eff122a41eb5c0a38b2578ffede4f82d
Tharun1828 <91731118+Tharun1828@users.noreply.github.com>
2022-08-05 07:21:25 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -90,11 +90,11 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             parse_mode="Markdown", disable_web_page_preview=True)
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=HMTD-Links_{str_to_b64(file_er_id)}"
+        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=SkMedia{str_to_b64(file_er_id)}"
         await editable.edit(
-            f"**Video Link:\n\n{share_link}**\n\n"
-            f"**Just Click the link to get your Videos**. **",
-            parse_mode="Markdown",
+            "**Your File Stored in my Database!**\n\n"
+            f"Here is the Permanent Link of your file: {share_link} \n\n"
+            "Just Click the link to get your file!",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
                  [InlineKeyboardButton("Bot Channel", url="https://t.me/HMTD_Links"),
